3e8909cd49c4b02836de89879c7eb3dabca3f9fc
Felix Chen <felix.chen19930112@gmail.com>
2024-10-14 19:54:27 +0200

    Add tailwindcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Adjust the path based on where your components are located
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
